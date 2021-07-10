38a3f5b769f61c3282ea7f1160c1843e40c1ad87
Vinícius Bessa <viniciusmoreira92@live.com>
2021-07-10 16:40:57 -0300

    Tela de splash - Conf imagens, cores e textos

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: const SplashPage(),
+    );
+  }
+}
